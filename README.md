b44047e7f6811ab48a54411fb8fd1822c541d8f5
DeveloperDurp <developerdurp@durp.info>
2022-10-18 19:02:39 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Simple powershell script to create an api endpoint to pull the PSU data from unraid
 
+Endpoints
+
+Health | Health Status
+unraid | PSU Output
+end | kill process
+
 ![image](https://user-images.githubusercontent.com/74198206/196565913-315ca0dc-a577-4ee7-9262-7af98f2d485c.png)
---
+++ pwsh-api.ps1
@@ -0,0 +1,72 @@
+param(
+    $password,
+    $Unraid_URI    
+)
+# Create a listener on port 8000
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add('http://+:8000/') 
+$listener.Start()
+'Listening ...'
+
+# Run until you send a GET request to /end
+while ($true) {
+    $context = $listener.GetContext() 
+
+    # Capture the details about the request
+    $request = $context.Request
+
+    # Setup a place to deliver a response
+    $response = $context.Response
+
+    # Break from loop if GET request sent to /end
+    if ($request.Url -match '/end$') { 
+        break 
+    } 
+    else {
+
+    # Split request URL to get command and options
+    $requestvars = ([String]$request.Url).split("/");        
+
+    # If a request is sent to http:// :8000/wmi
+    if ($requestvars[3] -eq "unraid") {
+
+        Write-Output "Getting unraid PSU data"
+        Invoke-WebRequest https://$Unraid_URI/login -SessionVariable unraid -Method Post -Body @{username='root';password="$Password"} | Out-Null;
+
+        # Get the class name and server name from the URL and run get-WMIObject
+        $result = Invoke-RestMethod https://$Unraid_URI/plugins/corsairpsu/status.php -WebSession $unraid;
+
+        # Convert the returned data to JSON and set the HTTP content type to JSON
+        $message = $result | convertto-json; 
+        $response.ContentType = 'application/json';
+
+    } 
+    elseif ($requestvars[3] -eq "Health"){
+
+        Write-Output "HealthCheck"
+        $Message = "OK"      
+
+    }
+    else {
+
+        # If no matching subdirectory/route is found generate a 404 message
+        $message = "This is not the page you're looking for.";
+        $response.ContentType = 'text/html' ;
+
+    }
+
+    # Convert the data to UTF8 bytes
+    [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+
+    # Set length of response
+    $response.ContentLength64 = $buffer.length
+
+    # Write response out and close
+    $output = $response.OutputStream
+    $output.Write($buffer, 0, $buffer.length)
+    $output.Close()
+    }    
+}
+
+#Terminate the listener
+$listener.Stop()
